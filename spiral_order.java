92700139cd0fceab0043da8fcdbb4d4fc6766733
shantanu17 <shantanushinde3292@gmail.com>
2025-09-13 13:09:17 +0530

    First commit

---
+++ spiral_order.java
@@ -0,0 +1,98 @@
+import java.util.*;
+
+public class spiral_order
+{
+public static void main(String ss[])
+{
+   Scanner sc = new Scanner(System.in);
+
+   int n = 4;
+   int m = 4;
+   
+   int arr[][] = new int[n][m];
+   
+   System.out.println("Enter array elements");
+	  for(int i=0;i<arr.length;i++)
+	  {
+	    for(int j=0;j<arr[i].length;j++)
+		{
+		   arr[i][j] = sc.nextInt();
+		}
+	  }
+	  
+	  
+	  System.out.println("Display array");
+	  for(int i=0;i<arr.length;i++)
+      {
+	    for(int j=0;j<arr[i].length;j++)
+		{
+		   System.out.print(arr[i][j]+" ");
+		}
+		System.out.println();
+	  }
+	  
+	  
+	  
+	  
+	  int top = 0;
+	  int left = 0;
+	  int right = n-1;
+	  int bottom = m-1;
+	  
+	  ArrayList<Integer> list = new ArrayList<>();
+	  
+	 
+    // outer loop checks that , even if single row/col is remainning for traverse we need to run loop	 
+	while(left<= right && top<=bottom)
+	{
+	  
+	  
+	  //1.left ---> right
+	  for(int i=left;i<=right;i++)
+	  {
+		  list.add(arr[top][i]);
+	  }
+	  top++;
+	  
+	  
+	  //2 . top---->bottom
+	  for(int i=top;i<=bottom;i++)
+	  {
+		  list.add(arr[i][right]);
+	  }
+	  right--;
+	  
+	  
+	  //3.right---->left
+	  for(int i=right;i>=left;i--)
+	  {
+		  list.add(arr[bottom][i]);
+	  }
+	  bottom--;
+	  
+	  
+	  //4.bottom--->top
+	  for(int i=bottom;i>=top;i--)
+	  {
+		  list.add(arr[i][left]);
+	  }
+	  left++;
+	  
+	}
+	
+	
+	// print the list , which contains the spiral order ..
+	for(int i=0;i<list.size();i++)
+	{
+		System.out.print(list.get(i)+" ");
+	}
+	  
+	  
+	  
+	  
+
+
+
+}
+
+}
